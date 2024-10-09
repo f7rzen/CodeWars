7b88d204dfc67e847f6533f5cb5ddf60c18a2b9d
Artem <akudinov04@mail.ru>
2024-10-09 21:47:15 +0300

    codewars

---
+++ kata/Multiples of 3 or 5.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func Multiple3And5(number int) int {
+	result := 0
+	for i := 1; i < number; i++ {
+		if i%3 == 0 || i%5 == 0 {
+			result += i
+		}
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(Multiple3And5(10))
+}
